93c8d20734aacadff1ca5bfdd2db0c51e6a2707f
Ariel Toom <ariel.toom@gmail.com>
2024-06-07 09:30:29 +0000

    setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloworld</h1>
+      <NavBar />
     </div>
   );
 }
