e6b0be48d7ceb196a7f75f535a73dc632c27f06b
Ariel Toom <ariel.toom@gmail.com>
2024-06-28 10:26:53 +0000

    message for creating comment

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import Alert from "react-bootstrap/Alert";
 
 function CommentCreateForm(props) {
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const handleChange = (event) => {
     setContent(event.target.value);
@@ -36,36 +36,49 @@ function CommentCreateForm(props) {
         ],
       }));
       setContent("");
+      setShowSuccessMessage(true);
+
+      const hideSuccessMessage = () => {
+        setShowSuccessMessage(false);
+      };
+      setTimeout(hideSuccessMessage, 3000);
     } catch (err) {
       // console.log(err);
     }
   };
 
+  
+
   return (
-    <Form className="mt-2" onSubmit={handleSubmit}>
-      <Form.Group>
-        <InputGroup>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profileImage} />
-          </Link>
-          <Form.Control
-            className={styles.Form}
-            placeholder="my comment..."
-            as="textarea"
-            value={content}
-            onChange={handleChange}
-            rows={2}
-          />
-        </InputGroup>
-      </Form.Group>
-      <button
-        className={`${styles.Button} btn d-block ml-auto`}
-        disabled={!content.trim()}
-        type="submit"
-      >
-        post
-      </button>
-    </Form>
+    <>
+      {showSuccessMessage && (
+        <Alert variant="success">You created a comment!</Alert>
+      )}
+      <Form className="mt-2" onSubmit={handleSubmit}>
+        <Form.Group>
+          <InputGroup>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profileImage} />
+            </Link>
+            <Form.Control
+              className={styles.Form}
+              placeholder="my comment..."
+              as="textarea"
+              value={content}
+              onChange={handleChange}
+              rows={2}
+            />
+          </InputGroup>
+        </Form.Group>
+        <button
+          className={`${styles.Button} btn d-block ml-auto`}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          post
+        </button>
+      </Form>
+    </>
   );
 }
 
